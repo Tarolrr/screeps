e8734d08911a16def2ad334275c1b793b59f83a1
tarolrr <tarolrr@gmail.com>
2024-12-29 23:26:17 +0100

    add script to send code to screeps server

---
+++ copyScripts.ps1
@@ -0,0 +1,4 @@
+$sourceDir = ".\dist\*"
+$destinationDir = "C:\Users\tarolrr\AppData\Local\Screeps\scripts\screeps.com\main"
+
+Copy-Item -Path $sourceDir -Destination $destinationDir -Recurse -Force
